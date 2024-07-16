7de80506e0841f0ec79515eb9fa8bba394a8ece9
Roei <roygim@gmail.com>
2024-07-16 14:40:11 +0300

    roeig

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,6 @@
 // The ApolloServer constructor requires two parameters: your schema
-
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from '@apollo/server/standalone';
-
 const typeDefs = `#graphql
   type Book {
     title: String
@@ -13,36 +11,31 @@ const typeDefs = `#graphql
     books: [Book]
   }
 `;
-
 const books = [
-  {
-    title: 'The Awakening',
-    author: 'Kate Chopin',
-  },
-  {
-    title: 'City of Glass',
-    author: 'Paul Auster',
-  },
+    {
+        title: 'The Awakening',
+        author: 'Kate Chopin',
+    },
+    {
+        title: 'City of Glass',
+        author: 'Paul Auster',
+    },
 ];
-
 const resolvers = {
-  Query: {
-    books: () => books,
-  },
+    Query: {
+        books: () => books,
+    },
 };
-
 // definition and your set of resolvers.
 const server = new ApolloServer({
-  typeDefs,
-  resolvers,
+    typeDefs,
+    resolvers,
 });
-
 // Passing an ApolloServer instance to the `startStandaloneServer` function:
 //  1. creates an Express app
 //  2. installs your ApolloServer instance as middleware
 //  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(server, {
-  listen: { port: 4000 },
+    listen: { port: 4000 },
 });
-
 console.log(`🚀  Server ready at: ${url}`);
