4300c7118765574aa8d54bcde1897b2b7f6df50e
Roei <roygim@gmail.com>
2024-07-16 15:57:22 +0300

    roeig

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
-// The ApolloServer constructor requires two parameters: your schema
-
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from '@apollo/server/standalone';
-import { typeDefs } from './schema';
-import { authors, books } from "./_db";
+import { typeDefs } from './schema.js';
+import { authors, books } from "./_db.js";
 
 const resolvers = {
   Query: {
@@ -12,16 +10,11 @@ const resolvers = {
   },
 };
 
-// definition and your set of resolvers.
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-// Passing an ApolloServer instance to the `startStandaloneServer` function:
-//  1. creates an Express app
-//  2. installs your ApolloServer instance as middleware
-//  3. prepares your app to handle incoming requests
 const { url } = await startStandaloneServer(server, {
   listen: { port: 4000 },
 });
